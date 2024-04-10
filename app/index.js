c01eaf8facca840abb58b24f197855415ccbfe61
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-04-10 20:14:25 +0530

    welcome page started

--- app/index.js
+++ app/index.js
@@ -1,11 +1,43 @@
-import { View , Text} from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants/index";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components/index";
 
-const Home = () => { 
-    return (
-        <View>
-            <Text>Home</Text>
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      {/* navigation bar */}
+
+      <Stack.Screen
+        options={{
+          headerLargeStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
         </View>
-    );
-}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -11,4 +11,4 @@ const Welcome = () => {
   )
 }
 
-export default Welcome
+export default Welcome;
