8b3f5b87e292aa70f2074af9b973c30e50f94c54
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-04-22 13:27:16 +0530

    popular cards.js done

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -15,7 +15,7 @@ import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const { data, isLoading, error } = useFetch("search", {
+  const { data, isLoading, error } = useFetch('search', {
     query: "React developer",
     num_pages: "1",
   })
@@ -38,8 +38,8 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4, 5, 6, 7, 8]}
-            renderItem={({ item }) => <PopularJobCard item={item} />}
+            data={[1, 2, 3, 4, 5]}
+            renderItem={({ item }) => <PopularJobCard item={item}/>}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal={true}
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -7,45 +7,45 @@ const useFetch = (endpoint , query) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  //KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA
+  // 7892659a19msh5ce4e1f20cfca36p1c7aaajsna133c75b3068
+
   const options = {
     method: "GET",
-    url: `https://jsearch.p.rapidapi.com/search/${endpoint}`,
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "7892659a19msh5ce4e1f20cfca36p1c7aaajsna133c75b3068",
+      "X-RapidAPI-Key": "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: {
       ...query,
     },
   };
-    
-    const fetchData = async () => { 
 
-        setIsLoading(true);
+  const fetchData = async () => {
+    setIsLoading(true);
 
-        try { 
-            const response = await axios.request(options);
-            setData(response.data.data);
-            setIsLoading(false);
-        }
-        catch (error) {
-            setError(error);
-            alert("There is an error");
-        }
-        finally {
-            setIsLoading(false);
-        }
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const reFetch =  () => {
-        setIsLoading(true);
-        fetchData();
-    }
-    return { data, isLoading, error, reFetch };
+  const reFetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+  return { data, isLoading, error, reFetch };
 };
 
 export default useFetch;
