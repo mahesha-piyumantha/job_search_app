6fb8a5c6002af23ef83c0f76914b19b082a3b2ed
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-04-23 00:02:07 +0530

    nearbyjobs page done

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,57 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from "react";
 
-import styles from './nearbyjobs.style'
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 
-const Nearbyjobs = () => {
+import styles from "./nearbyjobs.style";
+import { useRouter } from "expo-router";
+import { COLORS } from "../../../constants";
+import useFetch from "../../../hook/useFetch";
+import NearbyJobCard from "./../../common/cards/nearby/NearbyJobCard";
+
+const NearbyJobs = () => {
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
+
+  const [jobData, setJobData] = useState([]);
+
+  // Extract the job data from the API response
+  useEffect(() => {
+    if (data) {
+      setJobData(data);
+    }
+  }, [data]);
+
+  const limitedJobData = jobData.slice(0, 5);
+
   return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
 
-export default Nearbyjobs
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          limitedJobData?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+            />
+          ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default NearbyJobs;
