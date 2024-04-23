297e0fe99c5755163c5180a56599b4cd6da8f035
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-04-23 19:48:51 +0530

    making job details page

---
+++ app/job-details/[id].js
@@ -0,0 +1,10 @@
+import { Stack , useRouter , useSearchParams } from "expo-router";
+import { Text , View , SafeAreaView , ScrollView , ActivityIndicator , RefreshControl } from "react-native";
+
+const JobDetails = () => {
+  return (
+    <Text>job details route</Text>
+  )
+}
+
+export default JobDetails;
